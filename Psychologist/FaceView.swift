609e73fd570d34a883c5946a57dc3b2e0f0a2538
malcolmmacleod <malcolm@macleodhome.com>
2015-07-18 12:41:03 +0100

    Test commit

--- Psychologist/FaceView.swift
+++ Psychologist/FaceView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 protocol FaceViewDataSource : class {
-    func smilinessForFaceView(sender: FaceView) -> Double?
+    func smilinessForFaceView(sender: FaceView) -> Double? 
 }
 
 @IBDesignable
